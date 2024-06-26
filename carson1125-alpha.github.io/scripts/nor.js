83ce95725734f52ab8101425583bc216c7223d25
Carson Wu <topics.copilot0n@icloud.com>
2024-06-27 00:17:51 +0800

    v1.1.5.0(1150270624a)

--- carson1125-alpha.github.io/scripts/nor.js
+++ carson1125-alpha.github.io/scripts/nor.js
@@ -36,19 +36,3 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
-function openPopup(popupId) {
-    var popupOverlay = document.getElementById('popupOverlay' + popupId);
-    var popup = document.getElementById('popup' + popupId);
-  
-    popupOverlay.style.display = 'flex';
-  
-    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    var popupWidth = popup.offsetWidth;
-    var leftPosition = (screenWidth - popupWidth) / 2;
-    popup.style.left = leftPosition + 'px';
-}
-
-function closePopup(popupId) {
-    document.getElementById('popupOverlay' + popupId).style.display = 'none';
-}
