36346f889bb59608275fd067ca435876c2995db1
Carson Wu <topics.copilot0n@icloud.com>
2024-06-28 22:15:19 +0800

    v1.1.5.0(001)(1150_001-280624a)

--- carson1125-alpha.github.io/scripts/word.js
+++ carson1125-alpha.github.io/scripts/word.js
@@ -1,2 +1,2 @@
-document.getElementById("version").textContent = "v1.1.5.0(1150280624a)";
+document.getElementById("version").textContent = "v1.1.5.0(001)(1150_001-280624a)";
 document.getElementById("cr").textContent = "Copyright © 2024 Carson. All rights reserved.";
