9a05265953aa135f084878d52bb0251935794bf4
Carson Wu <topics.copilot0n@icloud.com>
2024-06-28 16:25:33 +0800

    v1.1.5.0(1150280624a)

--- carson1125-alpha.github.io/scripts/word.js
+++ carson1125-alpha.github.io/scripts/word.js
@@ -1,4 +1,2 @@
+document.getElementById("version").textContent = "v1.1.5.0(1150280624a)";
 document.getElementById("cr").textContent = "Copyright © 2024 Carson. All rights reserved.";
-document.getElementById("crzh").textContent = "Copyright © 2024 Carson 保留一切權利。";
-document.getElementById("crkr").textContent = "Copyright © 2024 Carson 모든 권리 보유.";
-document.getElementById("ve").textContent = "v1.1.5.0(1150280624r)";
