a2a67852e84cf21f798f19bea265beb1b9099e6b
Carson Wu <topics.copilot0n@icloud.com>
2024-06-28 21:37:04 +0800

    v1.1.5.0(1150280624a)

--- carson1125-alpha.github.io/scripts/blog.js
+++ carson1125-alpha.github.io/scripts/blog.js
@@ -28,3 +28,66 @@ var blogs = [
       link: "https://github.com/Carson-We/Documentation/blob/main/Blog/Blogs/2024/April/02_04_2024/NeuralMorse_kr.md"
     }
 ];
+
+var blogs_zh = [
+  {
+    title: "A.R.I.E.L.",
+    subtitle: "有關 A.R.I.E.L. 的技術部落格",
+    author: "Carson Wu",
+    date: "2024年4月7日",
+    link: "https://github.com/Carson-We/Documentation/blob/main/Blog/Blogs/2024/April/07_04_2024/Ariel_zh.md"
+  },
+
+    {
+      title: "K.R.I.S.T.Y.",
+      subtitle: "邏輯推理系統開發遭遇挫折",
+      author: "Carson Wu",
+      date: "2024年4月4日",
+      link: "https://github.com/Carson-We/Documentation/blob/main/Blog/Blogs/2024/April/04_04_2024/Kristy_zh.md"
+    },
+    {
+      title: "Carson1125",
+      subtitle: "有關 carson1125 'v0.1.0.2（1002304024b）' 更新的技術部落格",
+      author: "Carson Wu",
+      date: "2024年4月3日",
+      link: "https://github.com/Carson-We/Documentation/blob/main/Blog/Blogs/2024/April/03_04_2024/carson1125_zh.md"
+    },
+    {
+      title: "NeuralMorse",
+      subtitle: "有關 NeuralMorse 的技術部落格",
+      author: "Carson Wu",
+      date: "2024年4月2日",
+      link: "https://github.com/Carson-We/Documentation/blob/main/Blog/Blogs/2024/April/02_04_2024/NeuralMorse_zh.md"
+    }
+];
+
+var blogs_kr = [
+  {
+    title: "A.R.I.E.L.",
+    subtitle: "A.R.I.E.L.에 관한 기술 블로그",
+    author: "Carson Wu",
+    date: "2024년 4월 7일",
+    link: "https://github.com/Carson-We/Blog/blob/main/Blogs/2024/April/07_04_2024/Ariel_kr.md"
+  },
+    {
+      title: "K.R.I.S.T.Y.",
+      subtitle: "논리 추론 시스템 개발 지연 발표",
+      author: "Carson Wu",
+      date: "2024년 4월 4일",
+      link: "https://github.com/Carson-We/Documentation/blob/main/Blog/Blogs/2024/April/04_04_2024/Kristy_kr.md"
+    },
+    {
+      title: "Carson1125",
+      subtitle: "carson1125 'v0.1.0.2(1002304024b)' 업데이트에 대한 기술 블로그",
+      author: "Carson Wu",
+      date: "2024년 4월 3일",
+      link: "https://github.com/Carson-We/Blog/blob/main/Blogs/2024/April/03_04_2024/carson1125_kr.md"
+    },
+    {
+      title: "NeuralMorse",
+      subtitle: "NeuralMorse에 관한 기술 블로그",
+      author: "Carson Wu",
+      date: "2024년 4월 2일",
+      link: "https://github.com/Carson-We/Blog/blob/main/Blogs/2024/April/04_04_2024/Kristy_kr.md"
+    }
+];
