3f1bdfa6460187956543e2fc68367fafaf1df48a
Carson Wu <topics.copilot0n@icloud.com>
2024-06-30 01:06:41 +0800

    Delete pess-unify-beta.github.io directory

--- pess-unify-beta.github.io/unify-beta/scripts/js.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-            document.body.classList.add('loaded'); 
-        });
-
-        function goToHomePage() {
-            window.location.href = "index.html";
-        }
