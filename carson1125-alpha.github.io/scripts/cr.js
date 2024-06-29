6c06172ffbac06b06431e8821e61ffc76e99a718
Carson Wu <topics.copilot0n@icloud.com>
2024-06-30 01:06:26 +0800

    Delete carson1125-alpha.github.io directory

--- carson1125-alpha.github.io/scripts/cr.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("cr").textContent = "Copyright © 2024 Carson. All rights reserved.";
-
-document.getElementById("crzh").textContent = "Copyright © 2024 Carson 保留一切權利。";
-
-document.getElementById("crkr").textContent = "Copyright © 2024 Carson 모든 권리 보유.";
--- carson1125-alpha.github.io/scripts/version.js
+++
@@ -1,2 +0,0 @@
-var versionElement = document.getElementById("version");
-versionElement.textContent = "v1.1.5.0(1150280624a)";
