5fc0b3911174473115ad3dea1a9fea9f6bb5db30
Carson Wu <topics.copilot0n@icloud.com>
2024-07-05 19:47:21 +0800

    Delete carson1125/carson1125-mirror.github.io directory

--- carson1125/carson1125-mirror.github.io/scripts/word.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("version").textContent = "v1.1.5.2(006)(1152_006-050624r)";
-document.getElementById("cr").textContent = "Copyright © 2023-2024  Carson. All rights reserved.";
