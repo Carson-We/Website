a0cfbb3461a308bb00ff7d68e719e4b9ba618162
Carson Wu <topics.copilot0n@icloud.com>
2024-07-06 00:04:11 +0800

    v0.1.0.1(003)(0101_003-060724a)

--- Utility/Utility-alpha.github.io/scripts/word.js
+++ Utility/Utility-alpha.github.io/scripts/word.js
@@ -1,2 +1,2 @@
-document.getElementById("version").textContent = "v0.1.0.1(002)(0101_002-060724a)";
-document.getElementById("cr").textContent = "Copyright © 2024 Carson. All rights reserved.";
+document.getElementById("version").textContent = "v0.1.0.1(003)(0101_003-060724a)";
+document.getElementById("cr").textContent = "Copyright © 2024 Carson Utillty. All rights reserved.";
