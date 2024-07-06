9907c6ae949f40927ea40264cf7299cee78573b8
Carson Wu <topics.copilot0n@icloud.com>
2024-07-07 00:02:11 +0800

    v1.1.5.3(007)(1153_007-070624r)

--- carson1125/carson1125/scripts/word.js
+++ carson1125/carson1125/scripts/word.js
@@ -1,2 +1,19 @@
-document.getElementById("version").textContent = "v1.1.5.2(006)(1152_006-050624r)";
-document.getElementById("cr").textContent = "Copyright © 2023-2024  Carson. All rights reserved.";
+var versionElements = document.getElementsByClassName("version");
+for (var i = 0; i < versionElements.length; i++) {
+  versionElements[i].textContent = "v1.1.5.3(007)(1153_007-070624r)";
+}
+
+var crElements = document.getElementsByClassName("cr");
+for (var i = 0; i < crElements.length; i++) {
+  crElements[i].textContent = "Copyright © 2023-2024 Carson Wu. All rights reserved.";
+}
+
+var crKrElements = document.getElementsByClassName("crkr");
+for (var i = 0; i < crKrElements.length; i++) {
+  crKrElements[i].textContent = "Copyright © 2023-2024 Carson Wu 모든 권리 보유.";
+}
+
+var crZhElements = document.getElementsByClassName("crzh");
+for (var i = 0; i < crZhElements.length; i++) {
+  crZhElements[i].textContent = "Copyright © 2023-2024 Carson Wu 保留一切權利。";
+}
