4c2654938ff51b2f8c3298fc67dd624edaf86790
Carson Wu <topics.copilot0n@icloud.com>
2024-07-08 00:13:40 +0800

    v0.1.0.2(004)(0102_003-080724a)

--- Utility/Utility-alpha.github.io/scripts/nor.js
+++ Utility/Utility-alpha.github.io/scripts/nor.js
@@ -6,6 +6,38 @@ function goToHomePage() {
     window.location.href = "index.html";
 }
 
+function goToCR() {
+    window.location.href = "https://carson-we.github.io/Website/carson1125/carson1125/li.html";
+}
+
+function goToVE() {
+    window.location.href = "https://carson-we.github.io/Website/carson1125/carson1125/ve.html";
+}
+
+function goToHomePagezh() {
+    window.location.href = "index_zh.html";
+}
+
+function goToCRzh() {
+    window.location.href = "https://carson-we.github.io/Website/carson1125/carson1125/li_zh.html";
+}
+
+function goToVEzh() {
+    window.location.href = "https://carson-we.github.io/Website/carson1125/carson1125/ve_zh.html";
+}
+
+function goToHomePagekr() {
+    window.location.href = "index_kr.html";
+}
+
+function goToCRkr() {
+    window.location.href = "https://carson-we.github.io/Website/carson1125/carson1125/li_kr.html";
+}
+
+function goToVEkr() {
+    window.location.href = "https://carson-we.github.io/Website/carson1125/carson1125/ve_kr.html";
+}
+
 window.addEventListener('DOMContentLoaded', function() {
     var dropdownButton = document.querySelector('.dropbtn');
     var dropdownContent = document.querySelector('.dropdown-content');
--- Utility/Utility-alpha.github.io/scripts/word.js
+++ Utility/Utility-alpha.github.io/scripts/word.js
@@ -1,2 +1,19 @@
-document.getElementById("version").textContent = "v0.1.0.1(003)(0101_003-060724a)";
-document.getElementById("cr").textContent = "Copyright © 2024 Carson Utillty. All rights reserved.";
+var versionElements = document.getElementsByClassName("version");
+for (var i = 0; i < versionElements.length; i++) {
+  versionElements[i].textContent = "v0.1.0.2(004)(0102_003-080724a)";
+}
+
+var crElements = document.getElementsByClassName("cr");
+for (var i = 0; i < crElements.length; i++) {
+  crElements[i].textContent = "Copyright © 2024 Carson Utillty. All rights reserved.";
+}
+
+var crKrElements = document.getElementsByClassName("crkr");
+for (var i = 0; i < crKrElements.length; i++) {
+  crKrElements[i].textContent = "Copyright © 2024 Carson Utillty 모든 권리 보유.";
+}
+
+var crZhElements = document.getElementsByClassName("crzh");
+for (var i = 0; i < crZhElements.length; i++) {
+  crZhElements[i].textContent = "Copyright © 2024 Carson Utillty 保留一切權利。";
+}
