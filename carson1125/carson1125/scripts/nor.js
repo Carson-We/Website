65d358bdc8074b8d1e75904eddf51046407119b7
Carson Wu <topics.copilot0n@icloud.com>
2024-08-14 01:04:52 +0800

    v1.1.6.17(037)(11617_037-140824r)

--- carson1125/carson1125/scripts/nor.js
+++ carson1125/carson1125/scripts/nor.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   var versionElements = document.getElementsByClassName("version");
   for (var i = 0; i < versionElements.length; i++) {
-    versionElements[i].textContent = "v1.1.6.12(031)(11612_031-140824r)";
+    versionElements[i].textContent = "v1.1.6.17(037)(11617_037-140824r)";
   }
 
   var crElements = document.getElementsByClassName("cr");
