aff09231f7a342e733a04440eb16e60c9b34a9fd
Crason-We <topics.copilot0n@icloud.com>
2024-09-07 01:00:13 +0800

    v1.1.7.0(070)(1170_070-070924r)

--- carson1125/carson1125/BlazorApp/wwwroot/scripts/nor.js
+++ carson1125/carson1125/BlazorApp/wwwroot/scripts/nor.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const updateVersions = () => {
     document.querySelectorAll(".version").forEach((element) => {
-      element.textContent = "v1.1.7.0(069)(1170_069-070924r)";
+      element.textContent = "v1.1.7.0(070)(1170_070-070924r)";
     });
 
     document.querySelectorAll(".cr").forEach((element) => {
